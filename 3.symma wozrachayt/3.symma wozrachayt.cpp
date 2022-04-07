b822f7bfd5b453c38c344262c63897d6f731039b
Андрей Боярский <an0982978490@outlook.com>
2022-04-07 11:11:33 +0300

    Добавьте файлы проекта.

---
+++ 3.symma wozrachayt/3.symma wozrachayt.cpp
@@ -0,0 +1,18 @@
+﻿//3.	Написать функцию, которая получает в качестве параметров 2 целых числа и возвращает сумму чисел из диапазона между ними.
+#include<iostream>
+using namespace std;
+  int  SumDiapazona(int a, int b) {
+
+	int sum = 0;
+	if (a > b) swap(a, b);
+	for (int i = a; i <= b; i++)
+		sum += i;
+	return sum;
+}
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int a = 5, b = 11;
+	int sum = SumDiapazona(a, b);
+	cout << "Сумма чисел диапазона = " << sum << "\n";
+}
